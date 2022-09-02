3f6e5459c9fd257691ee8c3c21ce9a76c2f79d4d
romeoscript <romeobourne211@gmail.com>
2022-09-02 16:40:50 +0100

    achieved the required look for posts

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,27 @@ dropdown.addEventListener('mouseleave', () => {
   menu.style.display = 'none'
   drop.style.transform = 'rotate3d(0, 0, 1, 0deg)'
 })
+function getImagePreview(event) {
+  var image = URL.createObjectURL(event.target.files[0]);
+  var imagediv = document.getElementById("preview");
+  var newimg = document.createElement("img");
+  imagediv.innerHTML = "";
+  newimg.src = image;
+  newimg.width = "250";
+  imagediv.appendChild(newimg);
+}
+
+let reviewBtn = document.querySelectorAll('.review_btn')
+let reviewPost = document.querySelectorAll('.review_post')
+
+reviewBtn.forEach((btn)=>{
+  btn.addEventListener('click',(e)=>{
+    reviewPost.forEach((post)=>{
+      if (e.target.id == post.id) {
+        post.classList.toggle("review_item");
+    } else {
+        post.classList.remove("review_item");
+    }
+    })
+  })
+})
